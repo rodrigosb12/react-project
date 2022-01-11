0faf825e9e5f3e545505c8d459d2c722403a8ee1
rodrigosb12 <robarbosa09@gmail.com>
2022-01-11 12:52:32 -0300

    Final Version

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { Article } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 import "./blog.css";
 
 function Blog() {
-  return <div></div>;
+  const date = "Sep 26, 2021";
+  const title = "GPT-3 and OpenAI is the Future. Let us explore how it is?";
+  return (
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={blog01} date={date} title={title} />
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+          <Article imgUrl={blog02} date={date} title={title} />
+          <Article imgUrl={blog03} date={date} title={title} />
+          <Article imgUrl={blog04} date={date} title={title} />
+          <Article imgUrl={blog05} date={date} title={title} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Blog;
